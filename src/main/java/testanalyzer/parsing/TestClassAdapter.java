e0a3e9c3d34c54912beabe4f86c01289e7a2ab29
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-06-02 16:15:37 +0300

    removed duplicate testclass name from json

--- src/main/java/testanalyzer/parsing/TestClassAdapter.java
+++ src/main/java/testanalyzer/parsing/TestClassAdapter.java
@@ -1,5 +1,7 @@
 package testanalyzer.parsing;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import testanalyzer.model.TestClassInfo;
 import testanalyzer.model.TestInfo;
 
@@ -26,6 +28,7 @@ public class TestClassAdapter {
 		return new TestInfo();
 	}
 
+	@JsonIgnore
 	public String getClassName() throws Exception {
 		return parser.getTestClassName();
 	}
