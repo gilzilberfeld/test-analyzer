b54160cbb0f77f66eef9d8a12c519739adf2dfd8
gilzilberfeld <gil.zilberfeld@gmail.com>
2020-10-13 16:21:35 +0300

    cleanup

--- src/main/java/testanalyzer/export/MultiFileManager.java
+++ src/main/java/testanalyzer/export/MultiFileManager.java
@@ -15,13 +15,11 @@ public class MultiFileManager {
 	private int classCounter = 0;
 	private int testCounter = 0;
 	private String id;
-	private ObjectMapper mapper;
 	
 	
 	public MultiFileManager(TestContainer testContainer, String id) {
 		this.testContainer = testContainer;
 		this.id = id;
-		mapper = new ObjectMapper();
 	}
 	
 	public MultiFileManager(TestContainer testContainer) {
@@ -31,40 +29,7 @@ public class MultiFileManager {
 	public RunFileInfo getRunInfoFile() throws Exception {
 		return new RunFileInfo(id, testContainer.path, testContainer.getTestClassCount());
 	}
-	public String getRunFileName() {
-		return "TA_" + id + "_Run.json";
-	}
-
-//	public String getRunFileContent() throws Exception {
-//		RunFileInfo rfc = 
-//		return rfc.toJson();
-//	}
-
-
-	public int getClassFileCount() {
-		return this.testContainer.getTestClassCount();
-	}
-
-	public String getNextClassFileName() {
-		classCounter++;
-		String result = "TA_" + id + "_Class_" + Integer.toString(classCounter) + ".json";
-		return result;
-	}
-
-	public String getNextClassFileContent() throws Exception {
-		return mapper.writeValueAsString(testContainer.testClasses.get(classCounter));
-	}
-
-	public String getNextTestFileName() {
-		testCounter++;
-		String result = "TA_" + id + "_Test_" + Integer.toString(testCounter) + ".json";
-		return result;
-	}
-
-	public String getNextTestFileContent() throws Exception, Exception {
-		return mapper.writeValueAsString(testContainer.testClasses.get(0).getInfoForTest(0));
-	}
-
+	
 	public ClassFileInfo getNextClassFile() throws Exception {
 		TestClassAdapter testClass = testContainer.testClasses.get(classCounter);
 		ClassFileInfo classFileInfo = new ClassFileInfo(id, classCounter, testClass);
